20e08cb17b7427138b78b78a21d48b946b98ae10
William Gardner <william@madebycounter.com>
2024-03-24 17:49:48 -0700

    Added ctf netcat challenge

---
+++ ctf-netcat/server.py
@@ -0,0 +1,72 @@
+import asyncio, socket, os
+
+files = os.listdir("files")
+
+HOST = ('localhost', 65432)
+
+help_msg = """ ~~ Command Help ~~
+  ls - list files
+  cat - read file
+  help - show this message
+  exit - disconnect"""
+
+async def handle_client(client):
+    try:
+        loop = asyncio.get_event_loop()
+        running = True
+
+        while running:
+            await loop.sock_sendall(client, "user@netcat-server ~ $ ".encode("utf8"))
+
+            request = (await loop.sock_recv(client, 255)).decode('utf8').replace("\n","").replace("\r","").lower()
+
+            response = "nuh uh (try help)"
+            cmd = request.split(" ")[0]
+
+            if cmd == "help":
+                response = help_msg
+
+            if cmd == "ls" or cmd == "dir":
+                response = "\n".join(files)
+            
+            if cmd == "exit":
+                response = "Goodbye!"
+                running = False
+
+            if cmd == "cat" or cmd == "open" or cmd == "read":
+                split = request.split(" ")
+                if len(split) <= 1:
+                    response = "Unknown file"
+                else:
+                    file = " ".join(split[1:])
+                    found = False
+
+                    for f in files:
+                        if f.lower() == file.lower():
+                            response = open("files/" + f, "r").read()
+                            found = True
+                    
+                    if not found:
+                        response = "Unknown file"
+
+            await loop.sock_sendall(client, (response + "\n").encode('utf8'))
+
+        client.close()
+    except Exception as e:
+        print("Terminated connection")
+
+async def run_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(HOST)
+    server.listen(8)
+    server.setblocking(False)
+
+    loop = asyncio.get_event_loop()
+
+    print("Listening at " + repr(HOST))
+
+    while True:
+        client, _ = await loop.sock_accept(server)
+        loop.create_task(handle_client(client))
+
+asyncio.run(run_server())
